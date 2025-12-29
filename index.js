ee253791a2b10c11719fd917069fad621c468ab4
AWAIS MAYO KING009 <rojxb268@gmail.com>
2025-12-29 08:29:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
+const {
+  default: makeWASocket,
+  useMultiFileAuthState
+} = require("@whiskeysockets/baileys");
+
 const express = require("express");
 const app = express();
+
+let sock;
+
+async function startSock() {
+  const { state, saveCreds } =
+    await useMultiFileAuthState("session");
+
+  sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: false
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+
+  sock.ev.on("connection.update", (u) => {
+    if (u.connection === "open") {
+      console.log("✅ WhatsApp Connected");
+    }
+  });
+}
 
 app.get("/", (req, res) => {
   res.send("AWAIS MANO CYBER BACKEND RUNNING");
 });
 
-app.get("/pair", (req, res) => {
-  res.json({
-    success: true,
-    message: "Backend theek chal raha hai",
-    owner: "AWAIS Mayo",
-    bot: "AWAIS MANO CYBER BOT"
-  });
+app.get("/pair", async (req, res) => {
+  try {
+    if (!sock) await startSock();
+
+    const number = req.query.number;
+    const code = await sock.requestPairingCode(number);
+
+    res.json({
+      success: true,
+      pairingCode: code,
+      owner: "AWAIS Mayo",
+      bot: "AWAIS MANO CYBER BOT"
+    });
+  } catch (e) {
+    res.json({
+      success: false,
+      error: "Try again after some time"
+    });
+  }
 });
 
 app.listen(process.env.PORT || 3000);
